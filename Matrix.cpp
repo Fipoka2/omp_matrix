95fc05b607991446f8cdf007c04c9304d2acdee3
fipoka2 <smnv96@yandex.ru>
2018-10-14 23:14:55 +0300

    - min, max, average

--- Matrix.cpp
+++ Matrix.cpp
@@ -4,7 +4,7 @@
 #include <omp.h>
 #include "Matrix.h"
 
-const unsigned short int THREADS = 4;
+const unsigned short int THREADS = 8;
 
 Matrix::Matrix(size_t str, size_t col) : size(str),colSize(col)
 {
@@ -22,23 +22,18 @@ Matrix::Matrix(size_t str, size_t col) : size(str),colSize(col)
 
 
 Matrix Matrix::getMaxElemMatrix(Matrix a, Matrix b, Matrix c) {
-    cout<< "common ";
     Matrix result(a.size, a.colSize);
     int i;
 
-    double start = omp_get_wtime();
     for(i=0; i<result.size;i++) {
         for(int j = 0; j<result.colSize; j++) {
             result.data[i][j] = std::max({a.data[i][j],b.data[i][j],c.data[i][j]});
         }
     }
-    double end = omp_get_wtime();
-    cout << (end - start)<< endl;
     return result;
 }
 
 Matrix Matrix::calculateByBlocks(Matrix a, Matrix b, Matrix c) {
-    cout<< "blocks ";
     Matrix result(a.size, a.colSize);
     int i;
     omp_set_nested(true);
@@ -50,43 +45,33 @@ Matrix Matrix::calculateByBlocks(Matrix a, Matrix b, Matrix c) {
             result.data[i][j] = std::max({a.data[i][j],b.data[i][j],c.data[i][j]});
         }
     }
-    double end = omp_get_wtime();
-    cout << (end - start) << endl;
     return result;
 }
 
 
 Matrix Matrix::calculateByColumns(Matrix a, Matrix b, Matrix c) {
-    cout<< "columns ";
     Matrix result(a.size, a.colSize);
     int j,i,v;
 
-    double start = omp_get_wtime();
     for(i=0; i<result.size;i++) {
         #pragma omp parallel for  private(j)
         for(j = 0; j<result.colSize; j++) {
             result.data[i][j] = std::max({a.data[i][j],b.data[i][j],c.data[i][j]});
         }
     }
-    double end = omp_get_wtime();
-    cout << (end - start) << endl;
     return result;
 }
 
 Matrix Matrix::calculateByLines(Matrix a, Matrix b, Matrix c) {
-    cout<< "lines ";
     Matrix result(a.size, a.colSize);
     int i;
 
-    double start = omp_get_wtime();
     #pragma omp parallel for shared(a,b,c)
     for(i=0; i<result.size;i++) {
         for(int j = 0; j<result.colSize; j++) {
             result.data[i][j] = std::max({a.data[i][j],b.data[i][j],c.data[i][j]});
         }
     }
-    double end = omp_get_wtime();
-    cout << (end - start) << endl;
     return result;
 }
 void Matrix::print() {
@@ -104,4 +89,4 @@ void Matrix::print() {
  * 3) с разными чанками
  * 4) с разными размерностями
  * 5) с разными schedule
- * все кроме 2 делаются с наилучшим распаралеливанием*/
+ * все кроме 1,2 делаются с наилучшим распаралеливанием*/
--- main.cpp
+++ main.cpp
@@ -3,19 +3,70 @@
 #include <omp.h>
 #include <cmath>
 #include "Matrix.h"
+#include <limits>
+
+struct m_time {
+    double min = std::numeric_limits<double>::max();
+    double average = 0;
+    double max = 0;
+};
+
+const size_t MATRIX_SIZE = 6000;
+const unsigned short int RUNS = 2;
 
 int main() {
-    size_t s = 4000;
-    size_t cl = 4000;
-    Matrix a(s,cl);
-    Matrix b(s,cl);
-    Matrix c(s,cl);
-    for (int i=0; i<10; i++) {
-        cout << i << " раз" << endl;
+    Matrix a(MATRIX_SIZE, MATRIX_SIZE);
+    Matrix b(MATRIX_SIZE, MATRIX_SIZE);
+    Matrix c(MATRIX_SIZE, MATRIX_SIZE);
+
+    m_time serial;
+    m_time lines;
+    m_time columns;
+    m_time blocks;
+
+    for (int i = 0; i < RUNS; i++) {
+        double times[4];
+        double start;
+        double end;
+        start = omp_get_wtime();
         Matrix res = Matrix::getMaxElemMatrix(a,b,c);
+        end = omp_get_wtime();
+        times[0] = end - start;
+        start = omp_get_wtime();
         Matrix line = Matrix::calculateByLines(a,b,c);
+        end = omp_get_wtime();
+        times[1] = end - start;
+        start = omp_get_wtime();
         Matrix column = Matrix::calculateByColumns(a,b,c);
+        end = omp_get_wtime();
+        times[2] = end - start;
+        start = omp_get_wtime();
         Matrix block = Matrix::calculateByBlocks(a,b,c);
+        end = omp_get_wtime();
+        times[3] = end - start;
+
+        serial.max = serial.max < times[0] ? times[0] : serial.max;
+        lines.max = lines.max < times[1] ? times[1] : lines.max;
+        columns.max = columns.max < times[2] ? times[2] : columns.max;
+        blocks.max = blocks.max < times[3] ? times[3] : blocks.max;
+
+        serial.min = serial.min > times[0] ? times[0] : serial.min;
+        lines.min = lines.min > times[1] ? times[1] : lines.min;
+        columns.min = columns.min > times[2] ? times[2] : columns.min;
+        blocks.min = blocks.min > times[3] ? times[3] : blocks.min;
+
+        serial.average += times[0];
+        lines.average += times[1];
+        columns.average += times[2];
+        blocks.average += times[3];
+
     }
+    serial.average /= RUNS;
+    lines.average /= RUNS;
+    columns.average /= RUNS;
+    blocks.average /= RUNS;
+
+    cout << "\n serial average: " << serial.average << "\n lines average: "
+    << lines.average << "\n columns average: " << columns.average << "\n blocks average: "<< blocks.average;
     return 0;
 }
